1ccaf57ea5d926007b26c3c8721a3bf665c4831d
Tanawin Jenkunnaphat <tanawin_ohm@hotmail.com>
2024-06-05 23:24:48 +0700

    fix/

--- src/main.ts
+++ src/main.ts
@@ -9,29 +9,27 @@ import { ErrorFilter } from './exception-filter/error.filter'
 import { HttpExceptionFilter } from './exception-filter/http-exception.filter'
 
 async function bootstrap() {
-  const port = process.env.PORT || 3030
+  const port = 3030
   const app = await NestFactory.create(AppModule)
   app.use(helmet())
   app.use(compression())
   app.enableCors()
   app.use(json({ limit: '100mb' }))
   const logLogic = await app.resolve<LogLogic>(LogLogic)
-  if (process.env.NODE_ENV !== 'production') {
-    const config = new DocumentBuilder()
-      .setTitle('EXAM FULL STACK API')
-      .setDescription('EXAM FULL STACK API Document')
-      .setVersion('1.0')
-      .setExternalDoc('Postman Collection', '/api/docs-json')
-      .addBearerAuth()
-      .build()
-    const document = SwaggerModule.createDocument(app, config)
-    SwaggerModule.setup('api/docs', app, document, {
-      swaggerOptions: {
-        tagsSorter: 'alpha',
-        operationsSorter: 'alpha'
-      }
-    })
-  }
+  const config = new DocumentBuilder()
+    .setTitle('EXAM FULL STACK API')
+    .setDescription('EXAM FULL STACK API Document')
+    .setVersion('1.0')
+    .setExternalDoc('Postman Collection', '/api/docs-json')
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha'
+    }
+  })
   app.useGlobalFilters(new ErrorFilter(logLogic))
   app.useGlobalFilters(new HttpExceptionFilter(logLogic))
   app.use(urlencoded({ extended: true, limit: '100mb' }))
